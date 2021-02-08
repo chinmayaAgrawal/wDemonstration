9a836ce895a7d8379331d662d47454458a7730b7
Chinmaya Agrawal <chinmaya953agrawal@gmail.com>
2021-02-08 20:34:51 +0530

    2nd commit

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+// select DOM 
+const menuBtn = document.querySelector('.btn-menu');
+
+const menuNav = document.querySelector('.menu-nav');
+
+const menuBranding = document.querySelector('.menu-branding');
+
+const menu = document.querySelector('.menu');
